3dcdb0af1557a09269d7ee527218c04d9fc00cc4
mukundangopalachary <mukundangopalachary@gmail.com>
2026-01-13 07:09:22 +0530

    nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,49 +1,57 @@
 return {
-  {
-  "nvim-tree/nvim-tree.lua",
-  version = "*", -- Recommended to use stable releases
-  lazy = false,  -- Tree should be available immediately
-  dependencies = {
-    "nvim-tree/nvim-web-devicons", -- Required for file icons
-    "MunifTanjim/nui.nvim",
-    "nvim-lua/plenary.nvim",
-  },
-  config = function()
-    -- RECOMMENDED: Disable netrw at the very start (prevents conflicts)
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
 
-    -- Setup with 2026 default-friendly options
-    require("nvim-tree").setup({
-      sort = {
-        sorter = "case_sensitive",
-      },
-      view = {
-        width = 30,
-        relativenumber = true, -- Shows relative line numbers in the tree
-      },
-      renderer = {
-        group_empty = true,
-        highlight_git = true,
-        icons = {
-          show = {
-            git = true,
-            file = true,
-            folder = true,
-          },
-        },
-      },
-      filters = {
-        dotfiles = false, -- Set to true to hide hidden files
-      },
-    })
+		dependencies = {
+			{
+				"nvim-tree/nvim-web-devicons",
+				lazy = false,
+			},
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+		},
 
-    -- Common Keybindings
-    vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Toggle NvimTree', silent = true})
-    vim.keymap.set('n', '<leader>ef', ':NvimTreeFocus<CR>', { desc = 'Focus NvimTree', silent = true })
-  end,
+		config = function()
+			-- Disable netrw (required)
+			vim.g.loaded_netrw = 1
+			vim.g.loaded_netrwPlugin = 1
 
-  },
+			-- Setup nvim-tree
+			require("nvim-tree").setup({
+				sort = {
+					sorter = "case_sensitive",
+				},
 
-  { "nvim-tree/nvim-web-devicons", opts = {} }
+				view = {
+					width = 30,
+					relativenumber = true,
+				},
+
+				renderer = {
+					group_empty = true,
+					highlight_git = true,
+
+					icons = {
+						webdev_colors = true, -- 🔥 REQUIRED for devicons
+						show = {
+							file = true,
+							folder = true,
+							folder_arrow = true,
+							git = true,
+						},
+					},
+				},
+
+				filters = {
+					dotfiles = false,
+				},
+			})
+
+			-- Keymaps
+			vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
+			vim.keymap.set("n", "<leader>ef", ":NvimTreeFocus<CR>", { silent = true })
+		end,
+	},
 }
