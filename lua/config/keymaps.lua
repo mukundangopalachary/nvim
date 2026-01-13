fe9c01dfaf2831e6aedb287e76132fa84d3e2b98
mukundangopalachary <mukundangopalachary@gmail.com>
2026-01-13 07:07:02 +0530

    keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,4 +11,13 @@ vim.cmd("set wrap")
 vim.cmd("set encoding=utf-8")
 vim.cmd("nnoremap ,, :let @/ = ''<CR>:nohlsearch<CR>")
 
+vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy UI" })
+
 vim.opt.guifont = "FiraCode Nerd Font:h12"
+vim.opt.clipboard = "unnamedplus"
+
+-- for moving between windowf
+vim.keymap.set("n", "<leader>h", "<C-w>h", { silent = true })
+vim.keymap.set("n", "<leader>j", "<C-w>j", { silent = true })
+vim.keymap.set("n", "<leader>k", "<C-w>k", { silent = true })
+vim.keymap.set("n", "<leader>l", "<C-w>l", { silent = true })
