dc766b05fb736a4871fa71790f7aabb2199342d7
mukundangopalachary <mukundangopalachary@gmail.com>
2026-02-06 20:34:48 +0530

    update:lsp-config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,101 +1,109 @@
 return {
-	{
-		"mason-org/mason.nvim",
-		opts = {},
-	},
+    -- Mason
+    {
+        "mason-org/mason.nvim",
+        opts = {},
+    },
 
-	{
-		"mason-org/mason-lspconfig.nvim",
-		dependencies = {
-			"mason-org/mason.nvim",
-			"neovim/nvim-lspconfig",
-		},
-		opts = {
-			ensure_installed = {
-				"bashls",
-				"clangd",
-				"cssls",
-				"elixirls",
-				"eslint",
-				"gopls",
-				"html",
-				"jdtls",
-				"lua_ls",
-				"pyright",
-				"rust_analyzer",
-				"taplo",
-				"yamlls",
-			},
-		},
-	},
+    -- Mason ↔ LSP bridge (THIS IS CRITICAL)
+    {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = {
+            "mason-org/mason.nvim",
+            "neovim/nvim-lspconfig",
+        },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "bashls",
+                    "clangd",
+                    "cssls",
+                    "elixirls",
+                    "eslint", -- maps to eslint-lsp
+                    "gopls",
+                    "html",
+                    "jdtls",
+                    "lua_ls",
+                    "pyright",
+                    "rust_analyzer",
+                    "taplo",
+                    "yamlls",
+                },
+            })
+        end,
+    },
 
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    -- LSP (NEW API ONLY)
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			vim.diagnostic.config({
-				virtual_text = true,
-				signs = true,
-				underline = true,
-			})
+            -- Diagnostics
+            vim.diagnostic.config({
+                virtual_text = true,
+                signs = true,
+                underline = true,
+            })
 
-			-- Global default capabilities
-			vim.lsp.config("*", {
-				capabilities = capabilities,
-			})
+            -- Default config for ALL servers
+            vim.lsp.config("*", {
+                capabilities = capabilities,
+            })
 
-			-- Server-specific config
-			vim.lsp.config("lua_ls", {
-				settings = {
-					Lua = {
-						diagnostics = { globals = { "vim" } },
-					},
-				},
-			})
+            -- lua_ls custom config
+            vim.lsp.config("lua_ls", {
+                settings = {
+                    Lua = {
+                        diagnostics = { globals = { "vim" } },
+                    },
+                },
+            })
 
-			-- Enable all installed servers
-			vim.lsp.enable({
-				"bashls",
-				"clangd",
-				"cssls",
-				"elixirls",
-				"eslint",
-				"gopls",
-				"html",
-				"lua_ls",
-				"pyright",
-				"rust_analyzer",
-				"taplo",
-				"yamlls",
-			})
+            -- eslint config (NO require("lspconfig"))
+            vim.lsp.config("eslint", {
+                settings = {
+                    format = true,
+                },
+            })
 
-			-- LSP keymaps
-			vim.api.nvim_create_autocmd("LspAttach", {
-				callback = function(args)
-					local buf = args.buf
-					local client = vim.lsp.get_client_by_id(args.data.client_id)
+            -- Enable servers (AFTER mason-lspconfig setup!)
+            vim.lsp.enable({
+                "bashls",
+                "clangd",
+                "cssls",
+                "elixirls",
+                "eslint",
+                "gopls",
+                "html",
+                "lua_ls",
+                "pyright",
+                "rust_analyzer",
+                "taplo",
+                "yamlls",
+            })
 
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = buf }, { desc = "documentation" })
-					vim.keymap.set("n", "D", vim.lsp.buf.definition, { buffer = buf }, { desc = "definition" })
-					vim.keymap.set(
-						"n",
-						"<leader>ca",
-						vim.lsp.buf.code_action,
-						{ buffer = buf },
-						{ desc = "code actions" }
-					)
+            -- Keymaps + format-on-save
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(args)
+                    local buf = args.buf
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
 
-					if client and client.supports_method("textDocument/formatting") then
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							buffer = buf,
-							callback = function()
-								vim.lsp.buf.format({ bufnr = buf })
-							end,
-						})
-					end
-				end,
-			})
-		end,
-	},
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = buf })
+                    vim.keymap.set("n", "D", vim.lsp.buf.definition, { buffer = buf })
+                    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = buf })
+
+                    if client and client.supports_method("textDocument/formatting") then
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                            buffer = buf,
+                            callback = function()
+                                vim.lsp.buf.format({ bufnr = buf })
+                            end,
+                        })
+                    end
+                end,
+            })
+        end,
+    },
 }
+
