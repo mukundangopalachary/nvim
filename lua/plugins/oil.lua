f22ac426b1154c337d2b17f2507698640a943ec6
mukundangopalachary <mukundangopalachary@gmail.com>
2026-05-09 17:21:59 +0530

    toggleterm

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,28 +1,31 @@
 return {
-  "stevearc/oil.nvim",
-  opts = {
-    default_file_explorer = true,
-    columns = { "icon" },
-    view_options = {
-      show_hidden = true,
-    },
-    -- Configuration for the floating window
-    float = {
-      padding = 2,
-      max_width = 80,
-      max_height = 20,
-      border = "rounded",
-      win_options = {
-        winblend = 0,
-      },
-    },
-  },
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function(_, opts)
-    require("oil").setup(opts)
-    
-    -- Open Oil in a floating window instead of taking over the buffer
-    vim.keymap.set("n", "<leader>e", function() require("oil").open_float() end, { desc = "Open Oil (Float)" })
-    vim.keymap.set("n", "<leader>ef", function() require("oil").open_float() end, { desc = "Oil Focus (Float)" })
-  end,
+	"stevearc/oil.nvim",
+	opts = {
+		default_file_explorer = true,
+		columns = { "icon" },
+		view_options = {
+			show_hidden = true,
+		},
+		-- Configuration for the floating window
+		float = {
+			padding = 2,
+			max_width = 80,
+			max_height = 20,
+			border = "rounded",
+			win_options = {
+				winblend = 0,
+			},
+		},
+	},
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function(_, opts)
+		require("oil").setup(opts)
+		-- Open Oil in a floating window instead of taking over the buffer
+		vim.keymap.set("n", "<leader>e", function()
+			require("oil").open_float()
+		end, { desc = "Open Oil (Float)" })
+		vim.keymap.set("n", "<leader>ef", function()
+			require("oil").open_float()
+		end, { desc = "Oil Focus (Float)" })
+	end,
 }
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,40 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+		require("toggleterm").setup({
+			-- Reduced size to occupy roughly 1/4th of the screen
+			size = 35,
+			open_mapping = [[<c-\>]],
+			hide_numbers = true,
+			shade_filetypes = {},
+			shade_terminals = true,
+			shading_factor = 2,
+			start_in_insert = true,
+			insert_mappings = true,
+			terminal_mappings = true,
+			persist_size = true,
+			direction = "vertical",
+			close_on_exit = true,
+			shell = vim.o.shell,
+		})
+
+		function _G.set_terminal_keymaps()
+			local opts = { buffer = 0 }
+			vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+			vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+			vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+			vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+			vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+			vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+			vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+		end
+
+		vim.api.nvim_create_autocmd("TermOpen", {
+			pattern = "term://*",
+			callback = function()
+				set_terminal_keymaps()
+			end,
+		})
+	end,
+}
