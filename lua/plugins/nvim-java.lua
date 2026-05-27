9b44f08906d82a0fc66f629653b0903c76edeb31
mukundangopalachary <mukundangopalachary@gmail.com>
2026-05-27 16:42:52 +0530

    tokyonight

--- lua/plugins/nvim-java.lua
+++ lua/plugins/nvim-java.lua
@@ -1,14 +1,15 @@
 return {
-	"nvim-java/nvim-java",
-	ft = "java",
-	config = function()
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  "nvim-java/nvim-java",
+  lazy = true,
+  ft = "java",
+  config = function()
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		require("java").setup({
-			capabilities = capabilities,
-			spring_boot_tools = { enable = true },
-			java_test = { enable = true },
-			java_debug_adapter = { enable = true },
-		})
-	end,
+    require("java").setup({
+      capabilities = capabilities,
+      spring_boot_tools = { enable = true },
+      java_test = { enable = true },
+      java_debug_adapter = { enable = true },
+    })
+  end,
 }
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -2,24 +2,143 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
+
   opts = {
     style = "storm",
     transparent = true,
     terminal_colors = true,
+
     styles = {
       comments = { italic = true },
       keywords = { italic = true },
       sidebars = "dark",
       floats = "transparent",
     },
+
     on_highlights = function(hl)
-      hl.Comment = { fg = "#7f89b3", italic = true }
-      hl.Cursor = { fg = "#0f1419", bg = "#7dcfff" }
+      -- ==================================================
+      -- BASE UI
+      -- ==================================================
+
+      hl.Normal = { fg = "#C8C5E6" }
+      hl.NormalNC = { fg = "#C8C5E6" }
+
+      hl.Cursor = {
+        fg = "#0f1419",
+        bg = "#7dcfff",
+      }
+
       hl.NormalFloat = { bg = "none" }
       hl.FloatBorder = { bg = "none" }
       hl.FloatTitle = { bg = "none" }
+
+      hl.WhichKeyNormal = { bg = "none" }
+      hl.WhichKeyBorder = { bg = "none" }
+      hl.WhichKeyTitle = { bg = "none" }
+
+      -- ==================================================
+      -- STANDARD HIGHLIGHTS
+      -- ==================================================
+
+      hl.Comment = {
+        fg = "#615A7A",
+        italic = true,
+      }
+
+      hl.Keyword = {
+        fg = "#A78BFA",
+        italic = true,
+      }
+
+      hl.Function = {
+        fg = "#8CB4FF",
+      }
+
+      hl.String = {
+        fg = "#E6A1C4",
+      }
+
+      hl.Type = {
+        fg = "#B8A1FF",
+      }
+
+      hl.Number = {
+        fg = "#E6A86A",
+      }
+
+      hl.Identifier = {
+        fg = "#C8C5E6",
+      }
+
+      -- ==================================================
+      -- TREESITTER
+      -- ==================================================
+
+      hl["@comment"] = {
+        fg = "#615A7A",
+        italic = true,
+      }
+
+      hl["@keyword"] = {
+        fg = "#A78BFA",
+        italic = true,
+      }
+
+      hl["@keyword.function"] = {
+        fg = "#A78BFA",
+        italic = true,
+      }
+
+      hl["@function"] = {
+        fg = "#8CB4FF",
+      }
+
+      hl["@function.call"] = {
+        fg = "#8CB4FF",
+      }
+
+      hl["@string"] = {
+        fg = "#E6A1C4",
+      }
+
+      hl["@type"] = {
+        fg = "#B8A1FF",
+      }
+
+      hl["@type.builtin"] = {
+        fg = "#B8A1FF",
+      }
+
+      hl["@number"] = {
+        fg = "#E6A86A",
+      }
+
+      hl["@property"] = {
+        fg = "#C8C5E6",
+      }
+
+      -- ==================================================
+      -- JAVA IMPORT / PACKAGE FIX
+      -- ==================================================
+
+      hl["@module"] = {
+        fg = "#7C7AA6",
+      }
+
+      hl["@module.java"] = {
+        fg = "#7C7AA6",
+      }
+
+      hl["@namespace"] = {
+        fg = "#7C7AA6",
+      }
+
+      hl["@namespace.java"] = {
+        fg = "#7C7AA6",
+      }
     end,
   },
+
   config = function(_, opts)
     require("tokyonight").setup(opts)
     vim.cmd.colorscheme("tokyonight-storm")
